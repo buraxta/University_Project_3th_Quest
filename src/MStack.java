44a3d80433759898fe5a98fa1606fb5dfc104dc7
Burak Güllüler <burakgulluler@gmail.com>
2022-12-10 21:26:03 +0300

    waiting to be approved

--- src/MStack.java
+++ src/MStack.java
@@ -1,38 +1,63 @@
 import java.util.Arrays;
 
 public class MStack {
-    private char[] arr;
-    private int maxSize = 10;
-    private int stackSize = 2;
-    private int[] stacksInArr;
+    private char[] arr;         //All stacks stored here
+    private int maxSize ;      //Max size of array
+    private int totalStack;    //How many stack users want
+    private int[] stacksInArr; //I use this array to store every stack's starting point
+    private int interval;      //I use this interval value in some calculations
 
-
-    public MStack(int maxSize, int stackSize){
+    public MStack(int maxSize, int totalStack){
         this.maxSize = maxSize;
-        this.stackSize = stackSize;
+        this.totalStack = totalStack;
         this.arr = new char[maxSize];
-        stacksInArr = new int[maxSize/stackSize];
+        interval = (maxSize / totalStack);
+        stacksInArr = new int[totalStack];
+        splitStack();
     }
 
-    private int interval = (this.maxSize / this.stackSize);
 
+    //I construct stacksInArr here (which I use this array to store every stack's starting point)
     public void splitStack(){
+        int splitter = interval;
         for (int i = 0; i<stacksInArr.length; i++){
-            stacksInArr[i] = interval;
-            interval+=interval;
+            stacksInArr[i] = splitter;
+            splitter+=interval;
         }
     }
 
     public void push(char ch, int stack){
-        int givenStack = stacksInArr[stack+1];
-        for (int i = givenStack; i<interval;i++){
-            if (arr[givenStack] == 0){
-                arr[givenStack] = ch;
+        int givenStack = stacksInArr[stack-1];
+        for (int i = givenStack-interval, x=0; x<interval;x++,i++){
+            if (arr[i] == 0){
+                arr[i] = ch;
                 return;
             }
         }
         System.out.println("Stack over flow");
     }
 
+    public void pop(int stack){
+        boolean flag = true;
+        int givenStack = stacksInArr[stack-1];
+        for (int i = givenStack-1, x=0; x<interval;x++,i--){
+            if (arr[i] != 0){
+                System.out.println(arr[i]);
+                arr[i] = 0;
+                flag = false;
+                return;
+            }
+        }
+        if (flag){
+            System.out.println("No more value in stack");
+        }
 
+    }
+
+    @Override
+    public String toString() {
+        return "MStack{" +
+                "arr=" + Arrays.toString(arr) +
+                '}';
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        MStack mStack = new MStack(10,2);
+        MStack mStack = new MStack(18,6);
         mStack.push('a',1);
-        mStack.push('c',2);
+        mStack.push('b',1);
+        mStack.push('a',2);
+        mStack.push('b',2);
+        mStack.push('a',3);
+        mStack.push('b',3);
+        mStack.push('a',4);
+        mStack.push('b',4);
+        mStack.push('a',5);
+        mStack.push('b',5);
+        mStack.push('a',6);
+        mStack.push('b',6);
+        System.out.println("Before popped " + mStack);
+        mStack.pop(1);
+        System.out.println("After popped " + mStack);
     }
 }
